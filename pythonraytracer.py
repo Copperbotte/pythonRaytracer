397490c698213310aaa79c640a143d5a4a449b7c
Joe Kessler <copperbotte@gmail.com>
2020-07-08 10:43:36 -0700

    Added traverse, cleaned up helper functions

--- pythonraytracer.py
+++ pythonraytracer.py
@@ -3,17 +3,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
 
-class ray:
+class Ray:
     def __init__(self):
         self.src = np.array([0,0,0])
         self.vec = np.array([0,0,0])
 
+def dot(v1, v2):
+    v1 = np.array([v1])
+    v2 = np.array([v2])
+    return v1.dot(v2.transpose())[0][0]
+
 def length(vec):
-    vec = np.array([vec])
-    return np.sqrt(vec.dot(vec.transpose()))
+    return np.sqrt(dot(vec, vec))
 
 def normalize(vec):
     return vec / length(vec)
+
+def traverse(scene, ray):
+    outRay = Ray()
+    outRay.src = None
+    outRay.vec = ray.vec
+    
+    #floor
+    norm = np.array([0,0,1])
+    offset = -1
+    if offset < dot(norm, ray.src):
+        #above surface
+        proj = dot(norm, ray.vec)
+        if proj < 0:
+            outRay.src = ray.src - ray.vec / proj
+    
+    return outRay
 
 def render(width=800, height=600):
     x = np.arange(width)
@@ -21,17 +41,27 @@ def render(width=800, height=600):
     X = (2.0*x / width) - 1.0
     Y = (2.0*y / height) - 1.0
     Y *= height / width
-
+    
     img = []
-    for yPos in Y:
+    for yPos in Y[::-1]:
         line = []
         for xPos in X:
-            Z = np.sin(np.pi * 2.0 * xPos) * np.sin(np.pi * 2.0 * yPos)
+            ray = Ray()
+            ray.src = np.array([0,0,0])
+            ray.vec = normalize(np.array([xPos, 1.0, yPos]))
+
+            outRay = traverse(None, ray)
+            if outRay.src is None:
+                line.append([0.0,0.0,0.0])
+                continue
+
+            Z = np.sin(np.pi * 2.0 * outRay.src[0]) * np.sin(np.pi * 2.0 * outRay.src[1])
             line.append([Z,Z,Z])
         img.append(line)
     plt.imshow(img)
     plt.show()
     
-    
+if __name__ == "__main__":
+    render()
 
     
