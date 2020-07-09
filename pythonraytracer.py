4a3334345e763b3a70be5c367a0f6102f01470ec
Joe Kessler <copperbotte@gmail.com>
2020-07-08 18:42:50 -0700

    Added sphere rendering, cleaned up plane rendering

--- pythonraytracer.py
+++ pythonraytracer.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rnd
+import sys
 
 class Ray:
     def __init__(self):
@@ -23,16 +24,41 @@ def traverse(scene, ray):
     outRay = Ray()
     outRay.src = None
     outRay.vec = ray.vec
+    rayDist = sys.float_info.max
     
     #floor
     norm = np.array([0,0,1])
-    offset = -1
+    offset = dot(norm, np.array([0,0,-1]))
     if offset < dot(norm, ray.src):
         #above surface
         proj = dot(norm, ray.vec)
         if proj < 0:
-            outRay.src = ray.src - ray.vec / proj
+            dst = Ray()
+            dst.src = ray.src - ray.vec / proj
+            
+            dstlen = length(dst.src - ray.src)
+            if dstlen < rayDist:
+                None
+                rayDist = dstlen
+                outRay = dst
+                
+    #spheres
+    #project sphere's origin onto the ray
+    sPos = np.array([-0.25, 2.0, 0.0])
+    sRad = 0.3333
     
+    sOffset = sPos - ray.src
+    sRayClosest = dot(sOffset, ray.vec)
+    sRayClosestDist2 = dot(sOffset, sOffset) - sRayClosest**2
+    sInnerOffset2 = sRad**2 - sRayClosestDist2
+    if 0 < sInnerOffset2:
+        #hit sphere, calculate intersection
+        sInnerOffset = np.sqrt(sInnerOffset2)
+        rDist = sRayClosest - sInnerOffset
+        if rDist < rayDist:
+            rayDist = rDist
+            outRay.src = ray.src + ray.vec * rayDist
+            
     return outRay
 
 def render(width=800, height=600):
@@ -54,9 +80,12 @@ def render(width=800, height=600):
             if outRay.src is None:
                 line.append([0.0,0.0,0.0])
                 continue
-
-            Z = np.sin(np.pi * 2.0 * outRay.src[0]) * np.sin(np.pi * 2.0 * outRay.src[1])
-            line.append([Z,Z,Z])
+            if outRay.src[2] < -0.9:
+                Z = np.sin(np.pi * 2.0 * outRay.src[0]) * np.sin(np.pi * 2.0 * outRay.src[1])
+                line.append([Z,Z,Z])
+            else:
+                line.append([1.0,0.5,0.0])
+            
         img.append(line)
     plt.imshow(img)
     plt.show()
